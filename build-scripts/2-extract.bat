4cf93f88c98dc4a46ceba29ef3441724cb3a1af2
LinuxDonald <linuxdonald@posteo.de>
2019-01-16 16:20:45 +0100

    try again

--- build-scripts/2-extract.bat
+++ build-scripts/2-extract.bat
@@ -17,7 +17,7 @@ echo.
 
 echo.
 echo Fifengine Python36 Installer
-%ZIP% x "%CD%\fife\Python36.zip" -o%TARGET_DIR%
+%ZIP% x "C:\projects\uh-installer\fife\Python36.zip" -o%TARGET_DIR%
 
 echo.
 echo === Extracting Unknown-Horizons into the folder %TARGET_DIR%
