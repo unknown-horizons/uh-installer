38227aa508d13ea8f87ecd6f3a2b1451e377e753
LinuxDonald <linuxdonald@posteo.de>
2019-01-16 16:13:53 +0100

    fix typo

--- build-scripts/2-extract.bat
+++ build-scripts/2-extract.bat
@@ -17,7 +17,7 @@ echo.
 
 echo.
 echo Fifengine Python36 Installer
-%ZIP% x fife\Python3.6.zip -o%TARGET_DIR%
+%ZIP% x fife\Python36.zip -o%TARGET_DIR%
 
 echo.
 echo === Extracting Unknown-Horizons into the folder %TARGET_DIR%
