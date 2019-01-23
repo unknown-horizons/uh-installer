33ec8588cc9d62f56b4b8378ff248dceab7fd4cd
Jens A. Koch <jakoch@web.de>
2019-01-23 23:30:27 +0100

    xcopy instead of move

--- build-scripts/3-copy.bat
+++ build-scripts/3-copy.bat
@@ -17,19 +17,17 @@ pushd extracted
 
 if not exist %TARGET_DIR% (mkdir %TARGET_DIR%)
 
-echo Python36
-rem include Python from Appveyor
+echo Copying Python36 (from Appveyor)
 xcopy /SIQY C:\Python36 "%TARGET_DIR%\Python36"
 
-echo Fife-Engine
-move "%EXTRACTED_DIR%\Python36\Lib\site-packages\fife" "%TARGET_DIR%\Python36\Lib\site-packages\fife"
-move "%EXTRACTED_DIR%\Python36\Lib\site-packages\libfife-0.4.2-py3.6.egg-info" "%TARGET_DIR%\Python36\Lib\site-packages\libfife-0.4.2-py3.6.egg-info"
-dir "%EXTRACTED_DIR%\Python36\Lib\site-packages\"
+echo Copying Fife
+xcopy /SIQY "%EXTRACTED_DIR%\Python36\Lib\site-packages\fife" "%TARGET_DIR%\Python36\Lib\site-packages\fife"
+xcopy /SIQY "%EXTRACTED_DIR%\Python36\Lib\site-packages\libfife-0.4.2-py3.6.egg-info" "%TARGET_DIR%\Python36\Lib\site-packages\libfife-0.4.2-py3.6.egg-info"
 
-echo Unknown-Horizons
-move "%EXTRACTED_DIR%\unknown-horizons" "%TARGET_DIR%"
+echo Copying Unknown-Horizons
+xcopy /SIQY "%EXTRACTED_DIR%\unknown-horizons" "%TARGET_DIR%"
 
-echo Copy needed dlls installer
+echo Copying VCRedistributable
 copy "%LIBS%\vc_redist.x86.exe" "%TARGET_DIR%"
 
 popd
