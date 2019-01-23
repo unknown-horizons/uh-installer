c7b8ce4693dc1289f69cd23d8b30ad3763d5d183
jakoch <jakoch@web.de>
2019-01-24 00:24:01 +0100

    use Python3.7

--- build-scripts/3-copy.bat
+++ build-scripts/3-copy.bat
@@ -15,12 +15,12 @@ if not exist %TARGET_DIR% (mkdir %TARGET_DIR%)
 
 pushd extracted
 
-echo Copying Python36 (from Appveyor)
-xcopy /SIQY C:\Python36 "%TARGET_DIR%\Python36"
+echo Copying Python37 (from Appveyor)
+xcopy /SIQY C:\Python37 "%TARGET_DIR%\Python37"
 
 echo Copying Fife
-xcopy /SIQY "%EXTRACTED_DIR%\Python36\Lib\site-packages\fife" "%TARGET_DIR%\Python36\Lib\site-packages\fife"
-copy "%EXTRACTED_DIR%\Python36\Lib\site-packages\libfife-0.4.2-py3.6.egg-info" "%TARGET_DIR%\Python36\Lib\site-packages"
+xcopy /SIQY "%EXTRACTED_DIR%\Python37\Lib\site-packages\fife" "%TARGET_DIR%\Python36\Lib\site-packages\fife"
+copy "%EXTRACTED_DIR%\Python37\Lib\site-packages\libfife-0.4.2-py3.6.egg-info" "%TARGET_DIR%\Python36\Lib\site-packages"
 
 echo Copying Unknown-Horizons
 xcopy /SIQY "%EXTRACTED_DIR%\unknown-horizons" "%TARGET_DIR%"
