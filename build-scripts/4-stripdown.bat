ab5c2ea690170488fbc949683575614dd27bc9b1
Jens A. Koch <jakoch@web.de>
2019-01-23 20:24:38 +0100

    move stuff from appveyor.yml into stripdown script

--- build-scripts/4-stripdown.bat
+++ build-scripts/4-stripdown.bat
@@ -11,8 +11,56 @@ echo.
 pushd repackage
 
 echo.
-echo Unknown-Horizons
-rm       "..\repackage\unknown-horizons\Mypy.ini"
-rm       "..\repackage\unknown-horizons\.editorconfig"
+echo === Stripping Unknown-Horizons
+echo.
+
+cd unknown-horizons
+
+rem Remove all .pyc and .pyo files
+del /S /Q *.pyc
+del /S /Q *.pyo
+del /Q unknown-horizons.wpr
+del /Q stage_build_mac.py
+del /Q CONTRIBUTING.md
+rd /S /Q tests
+  
+del /Q Mypy.ini
+del /Q .editorconfig
+del /Q .coveragerc
+del /Q .gitattributes
+del /Q .gitignore
+del /Q .isort.cfg
+del /Q .travis.yml
+
+rem Removing non-atlas graphics...
+rd /S /Q content\gfx\base
+rd /S /Q content\gfx\buildings
+rd /S /Q content\gfx\units
+rd /S /Q content\gfx\terrain
+
+rem Removing dev stuff - Not needed folder for gamer version of installer
+rd /S /Q development
+
+rem Remove translation artifacts
+del /S /Q "po\*.po~"
+
+rem These files/folders may or may not be present
+rem dir * /s/b | findstr __pycache__ | attrib +h +s +r
+rd /S /Q __pycache__
+
+cd..
+
+echo.
+echo === Stripping Python
+echo.
+
+cd Python36
+
+rd /S /Q Doc
+rd /S /Q Scripts
+rd /S /Q tcl
+rd /S /Q Tools
+
+cd..
 
 popd
